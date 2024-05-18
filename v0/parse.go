1d84df4536d506b8cf0cbccee404ef23e645d168
longlodw <longlodw@gmail.com>
2024-05-17 22:31:36 -0400

    change from io.Reader to byte array

--- v0/parse.go
+++ v0/parse.go
@@ -3,7 +3,6 @@ package v0
 import (
   "encoding/binary"
   "errors"
-  "io"
 )
 
 // constants
@@ -36,13 +35,8 @@ const (
   IntroductionStatusUnknownPeer = 2
 )
 
-func ParsePacketHeader(r io.Reader) (version byte, packetType byte, length uint16, err error) {
-  buffer := make([]byte, HeaderSize)
-  nums, err := r.Read(buffer)
-  if err != nil {
-    return 0, 0, 0, err
-  }
-  if nums != HeaderSize {
+func ParsePacketHeader(buffer []byte) (version byte, packetType byte, length uint16, err error) {
+  if len(buffer) < HeaderSize {
     return 0, 0, 0, errors.New("header size error")
   }
   version = buffer[0]
@@ -54,83 +48,37 @@ func ParsePacketHeader(r io.Reader) (version byte, packetType byte, length uint1
   return version, packetType, length, nil
 }
 
-func ParseHello(r io.Reader) (publicKeyDH [32]byte, publicKeyED [32]byte, dynamicIP bool, cookie [32]byte, signature [64]byte, err error) {
-  nums, err := r.Read(publicKeyDH[:])
-  if err != nil {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err
+func ParseHello(buffer []byte) (publicKeyDH []byte, publicKeyED []byte, dynamicIP bool, cookie []byte, signature []byte, err error) {
+  if len(buffer) < 129 {
+    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, errors.New("hello size error")
   }
-  if nums != 32 {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, errors.New("public key dh size error")
-  }
-
-  nums, err = r.Read(publicKeyED[:])
-  if err != nil {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err
-  }
-  if nums != 32 {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, errors.New("public key ed size error")
-  }
-
-  dynamicIPByte := make([]byte, 1)
-  nums, err = r.Read(dynamicIPByte)
-  if err != nil {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err
-  }
-  if nums != 1 {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, errors.New("dynamic ip size error")
-  }
-  dynamicIP = dynamicIPByte[0] == 1
-
-  nums, err = r.Read(cookie[:])
-  if err != nil {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err
-  }
-  if nums != 32 {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, errors.New("cookie size error")
-  }
-
-  nums, err = r.Read(signature[:])
-  if err != nil {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err
-  }
-  if nums != 64 {
-    return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, errors.New("signature size error")
-  }
+  publicKeyDH = buffer[:32]
+  publicKeyED = buffer[32:64]
+  dynamicIP = buffer[64] == 1
+  cookie = buffer[65:97]
+  signature = buffer[97:161]
   return publicKeyDH, publicKeyED, dynamicIP, cookie, signature, nil
 }
 
-func ParsePing(r io.Reader) (cookie [32]byte, err error) {
-  nums, err := r.Read(cookie[:])
-  if err != nil {
-    return cookie, err
+func ParsePing(buffer []byte) (cookie []byte, err error) {
+  if len(buffer) < 32 {
+    return cookie, errors.New("ping size error")
   }
-  if nums != 32 {
-    return cookie, errors.New("cookie size error")
-  }
+  cookie = buffer
   return cookie, nil
 }
 
 // when parsing data, we need to parse the data header first
 // using the data header, we can determine the stream id and data type and then parse the data
-func ParseDataHeader(r io.Reader) (streamID byte, dataType byte, err error) {
-  buffer := make([]byte, 2)
-  nums, err := r.Read(buffer)
-  if err != nil {
-    return 0, 0, err
-  }
-  if nums != 2 {
+func ParseDataHeader(buffer []byte) (streamID byte, dataType byte, err error) {
+  if len(buffer) < 2 {
     return 0, 0, errors.New("data header size error")
   }
   return buffer[0], buffer[1], nil
 }
 
-func ParseReliableDataHeader(r io.Reader) (length uint16, sequenceNumber uint32, ackNumber uint32, err error) {
-  buffer := make([]byte, 10)
-  nums, err := r.Read(buffer)
-  if err != nil {
-    return 0, 0, 0, err
-  }
-  if nums != 10 {
+func ParseReliableDataHeader(buffer []byte) (length uint16, sequenceNumber uint32, ackNumber uint32, err error) {
+  if len(buffer) < 10 {
     return 0, 0, 0, errors.New("reliable data header size error")
   }
   length = binary.BigEndian.Uint16(buffer)
@@ -139,25 +87,15 @@ func ParseReliableDataHeader(r io.Reader) (length uint16, sequenceNumber uint32,
   return length, sequenceNumber, ackNumber, nil
 }
 
-func ParseUnreliableDataHeader(r io.Reader) (length uint16, err error) {
-  buffer := make([]byte, 2)
-  nums, err := r.Read(buffer)
-  if err != nil {
-    return 0, err
-  }
-  if nums != 2 {
+func ParseUnreliableDataHeader(buffer []byte) (length uint16, err error) {
+  if len(buffer) < 2 {
     return 0, errors.New("unreliable data header size error")
   }
   return binary.BigEndian.Uint16(buffer), nil
 }
 
-func ParseUnreliableDataAckHeader(r io.Reader) (length uint16, ackNumber uint32, err error) {
-  buffer := make([]byte, 6)
-  nums, err := r.Read(buffer)
-  if err != nil {
-    return 0, 0, err
-  }
-  if nums != 6 {
+func ParseUnreliableDataAckHeader(buffer []byte) (length uint16, ackNumber uint32, err error) {
+  if len(buffer) < 6 {
     return 0, 0, errors.New("unreliable data ack header size error")
   }
   length = binary.BigEndian.Uint16(buffer)
@@ -165,41 +103,24 @@ func ParseUnreliableDataAckHeader(r io.Reader) (length uint16, ackNumber uint32,
   return length, ackNumber, nil
 }
 
-func ParseIntroductionRequest(r io.Reader) (publicKeyDH [32]byte, publicKeyED [32]byte, dynamicIP bool, cookie [32]byte, signature [64]byte, err error) {
-  return ParseHello(r)
+func ParseIntroductionRequest(buffer []byte) (publicKeyDH []byte, publicKeyED []byte, dynamicIP bool, cookie []byte, signature []byte, err error) {
+  return ParseHello(buffer)
 }
 
-func ParseIntroductionAck(r io.Reader) (status byte, err error) {
-  buffer := make([]byte, 1)
-  nums, err := r.Read(buffer)
-  if err != nil {
-    return 0, err
-  }
-  if nums != 1 {
-    return 0, errors.New("status size error")
+func ParseIntroductionAck(buffer []byte) (status byte, err error) {
+  if len(buffer) < 1 {
+    return 0, errors.New("introduction ack size error")
   }
   return buffer[0], nil
 }
 
-func ParseIntroduction(r io.Reader) (ipv6 [16]byte, port uint16, publicKeyDH [32]byte, publicKeyED [32]byte, dynamicIP bool, cookie [32]byte, signature [64]byte, err error) {
-  nums, err := r.Read(ipv6[:])
-  if err != nil {
-    return ipv6, port, publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err
+func ParseIntroduction(buffer []byte) (ipv6 []byte, port uint16, publicKeyDH []byte, publicKeyED []byte, dynamicIP bool, cookie []byte, signature []byte, err error) {
+  if len(buffer) < 146 {
+    return ipv6, port, publicKeyDH, publicKeyED, dynamicIP, cookie, signature, errors.New("introduction size error")
   }
-  if nums != 16 {
-    return ipv6, port, publicKeyDH, publicKeyED, dynamicIP, cookie, signature, errors.New("ipv6 size error")
-  }
-
-  buffer := make([]byte, 2)
-  nums, err = r.Read(buffer)
-  if err != nil {
-    return ipv6, port, publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err
-  }
-  if nums != 2 {
-    return ipv6, port, publicKeyDH, publicKeyED, dynamicIP, cookie, signature, errors.New("port size error")
-  }
-  port = binary.BigEndian.Uint16(buffer)
-  publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err = ParseHello(r)
+  ipv6 = buffer[:16]
+  port = binary.BigEndian.Uint16(buffer[16:])
+  publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err = ParseHello(buffer[18:])
   return ipv6, port, publicKeyDH, publicKeyED, dynamicIP, cookie, signature, err
 }
 
